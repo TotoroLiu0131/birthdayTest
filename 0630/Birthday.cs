f076aa8ff4f850f9aa7f134823a250d43c342245
Benjamin Liu Po-Ching <benjamin.liu@titansoft.com.sg>
2018-07-13 15:27:37 +0800

    initial

---
+++ 0630/Birthday.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _0630
+{
+    public class Birthday
+    {
+        public string IsTodayJoeyBirthday()
+        {
+            return GetToday().Month == 9 && GetToday().Day == 1 ? "Happy Birthday" : "No";
+        }
+
+        protected virtual DateTime GetToday()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ 0630Tests/BirthdayTests.cs
@@ -0,0 +1,51 @@
+﻿using _0630;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace _0630Tests
+{
+    [TestClass()]
+    public class BirthdayTests
+    {
+        private FakeBirthday _fakeBirthday = new FakeBirthday();
+
+        [TestMethod()]
+        public void today_is_joey_birthday()
+        {
+            GivenDate(new DateTime(2018, 9, 1));
+            TheResultShouldBe("Happy Birthday");
+        }
+
+        private void GivenDate(DateTime dateTime)
+        {
+            _fakeBirthday.SetDate(dateTime);
+        }
+
+        private void TheResultShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _fakeBirthday.IsTodayJoeyBirthday());
+        }
+
+        [TestMethod()]
+        public void today_is_not_joey_birthday()
+        {
+            GivenDate(new DateTime(2018, 1, 1));
+            TheResultShouldBe("No");
+        }
+    }
+
+    internal class FakeBirthday : Birthday
+    {
+        private DateTime _day;
+
+        protected override DateTime GetToday()
+        {
+            return _day;
+        }
+
+        public void SetDate(DateTime dateTime)
+        {
+            _day = dateTime;
+        }
+    }
+}
